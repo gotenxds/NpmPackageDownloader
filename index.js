f7290152f8ae7f6388a90dc3073302780deb7ed6
John Doe <johndoe@example.com>
2015-10-19 10:17:05 +0300

    Added commander options.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var winston = require('winston'),
+    program = require('commander');
+
+program
+    .version('0.0.1')
+    .option('-d', '--download [packages]', 'A list of space seperated [packages]')
+    .option('-dep', '--dependencies', 'Download dependencies')
+    .option('-devdep', '--dev-dependencies', 'Download dev dependencies')
+    .option('-av', '--all-versions', 'Download all versions of each package')
+    .parse(process.argv);
 
