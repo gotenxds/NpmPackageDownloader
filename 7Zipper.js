1bb716f25825a55bb844f6cb983cb464392670da
John Doe <johndoe@example.com>
2015-10-23 16:01:06 +0300

    Moved ziping logic to a specific file.

---
+++ 7Zipper.js
@@ -0,0 +1,11 @@
+let winston = require('winston'),
+    Zip = require('node-7z');
+
+module.exports = function (location) {
+    winston.info('7Ziping your files, Please wait...');
+
+    let zip = new Zip();
+
+    return zip.add(location + '\\npm-personalNumber.7z', location)
+            .then(() => winston.info(`Finished ziping!\n7Zip is wating for you in ${location}`))
+};
