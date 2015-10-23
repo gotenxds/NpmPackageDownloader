0caca30a6fbb75cf0ef1d10f21e6508044bfd8f0
John Doe <johndoe@example.com>
2015-10-23 16:37:17 +0300

    Clean up.

--- bin/npmDownload.js
+++ bin/npmDownload.js
@@ -1,3 +1,32 @@
 #!/usr/bin/env node
+"use strict";
+let program = require('commander'),
+    packages = require('../coercions/packagesCoercion'),
+    app = require("../index.js"),
+    winston = require('winston'),
+    winstonConfigurator = require('../winstonConfiguration');
 
-require("../index.js");
+//noinspection JSCheckFunctionSignatures
+program
+    .version('0.1.2')
+    .option('-p, --packages [packages]', 'A list of space seperated [packages].', packages)
+    .option('-d, --dependencies', 'Download dependencies.')
+    .option('-e, --devDependencies', 'Download dev dependencies.')
+    .option('-a, --allVersions', 'Download all versions of each package.')
+    .option('-o, --output [directory]', 'The output [directory].')
+    .option('-7, --zipIt', '7Zips the downloaded files.')
+    .parse(process.argv);
+
+if (!program.packages) {
+    winston.error("No npmPackage were given!");
+    process.exit();
+}
+
+if (!program.output) {
+    winston.error("No output directory were given!");
+    process.exit();
+}
+
+winstonConfigurator(program.outout);
+winston.info("Welcome to npm package downloader.");
+app(program.packages, program);
--- index.js
+++ index.js
@@ -1,190 +1,139 @@
 "use strict";
 let winston = require('winston'),
-    program = require('commander'),
+    zip = require('./7Zipper'),
     _ = require('lodash'),
     npm = require('./npmApi.js'),
     Promise = require('bluebird'),
-    semver = require('semver'),
-    Zip = require('node-7z'),
-    validUrl = require('valid-url'),
-    packages = require('./coercions/packagesCoercion');
+    versionUtils = require('./versionUtils');
 
-program
-    .version('0.1.2')
-    .option('-p, --packages [packages]', 'A list of space seperated [packages].', packages)
-    .option('-d, --dependencies', 'Download dependencies.')
-    .option('-e, --devDependencies', 'Download dev dependencies.')
-    .option('-a, --allVersions', 'Download all versions of each package.')
-    .option('-o, --output [directory]', 'The output [directory].')
-    .option('-7, --zipIt', '7Zips the downloaded files.')
-    .parse(process.argv);
+module.exports = function (npmPackages, opts) {
 
-if (!program.packages) {
-    winston.error("No npmPackage were given!");
-    process.exit();
-}
+    winston.info("Downloading -> " + npmPackages.map(p => p.name));
 
-if (!program.output) {
-    winston.error("No output directory were given!");
-    process.exit();
-}
+    let packagesIterator = npmPackages[Symbol.iterator]();
 
-winston.add(winston.transports.File, {
-    level:'Error',
-    filename:`${program.output}\errorLog.log`
-});
-winston.cli();
-winston.info("Welcome to npm package downloader.");
-winston.info("Downloading -> " + program.packages.map(p => p.name));
+    function nextPackage() {
+        let nextPackage = packagesIterator.next().value;
 
-let npmPackages = program.packages;
-let packagesIterator = npmPackages[Symbol.iterator]();
-
-function nextPackage() {
-    let nextPackage = packagesIterator.next().value;
-
-    if (nextPackage) {
-        downloadPackage(nextPackage.name);
-    } else {
-        winston.info("Finished downloading.");
-
-        if (program.zipIt) {
-            winston.info('7Ziping your files, Please wait...');
-
-            let zip = new Zip();
-
-            zip.add(program.output + '\\npm-personalNumber.7z', program.output)
-                .then(() => winston.info(`Finished ziping!\n7Zip is wating for you in ${program.output}`))
-                .then(close);
+        if (nextPackage) {
+            downloadPackage(nextPackage.name);
         } else {
-            close()
+            winston.info("Finished downloading.");
+
+            if (opts.zipIt) {
+                zip(opts.output).then(close);
+            } else {
+                close()
+            }
         }
     }
-}
 
-function close() {
-    winston.info("GoodBye.");
-    process.exit();
-}
-
-function downloadPackage(name) {
-    addVersionsFor(name)
-        .then(() => downloadTarBall(name))
-        .then(() => addDependencies(name))
-        .then(() => addDevDependencies(name))
-        .then(nextPackage)
-}
-
-function addVersionsFor(name) {
-    if (program.allVersions) {
-        winston.info(`Adding all versions of ${name}`);
-        return addAllVersions(name);
-    } else {
-        let npmPackage = _.find(npmPackages, {name: name});
-
-        return npm.getLatestVersionOf(name)
-            .then(version => npmPackage.versions.add(version));
-    }
-}
-
-function addDependencies(name) {
-    if (program.dependencies) {
-        winston.info(`Adding dependencies for ${name}`);
-
-        return npm.getDependenciesOf(name)
-            .then((dependencies) => {
-                let promises = [];
-                _.forOwn(dependencies, (v, p) => promises.push(addLatestSatisfyingVersionOf(p, v)));
-
-                return Promise.all(promises)
-            });
+    function downloadPackage(name) {
+        addVersionsFor(name)
+            .then(() => downloadTarBall(name))
+            .then(() => addDependencies(name))
+            .then(() => addDevDependencies(name))
+            .then(nextPackage)
     }
 
-    return Promise.resolve();
-}
-
-function addDevDependencies(name) {
-    if (program.devDependencies) {
-        winston.info(`Adding dev dependencies for ${name}`);
-
-        return npm.getDevDependenciesOf(name)
-            .then((dependencies) => {
-                let promises = [];
-                _.forOwn(dependencies, (v, p) => promises.push(addLatestSatisfyingVersionOf(p, v)));
-
-                return Promise.all(promises)
-            });
+    function close() {
+        winston.info("GoodBye.");
+        process.exit();
     }
 
-    return Promise.resolve();
-}
+    function addVersionsFor(name) {
+        if (opts.allVersions) {
+            winston.info(`Adding all versions of ${name}`);
+            return addAllVersions(name);
+        } else {
+            let npmPackage = _.find(npmPackages, {name: name});
 
-function addLatestSatisfyingVersionOf(packageName, ver) {
-    return npm.getVersionsOf(packageName)
-        .spread((name, versions) => {
-            let npmPackage = _.find(npmPackages, {name: packageName});
-            let latestSatisfyingVersion = getLatestSatisfyingVersion(versions, ver);
+            return npm.getLatestVersionOf(name)
+                .then(version => npmPackage.versions.add(version));
+        }
+    }
 
-            if (!npmPackage) {
-                addNewPackage(packageName, latestSatisfyingVersion);
-            } else {
-                if (!packageContainsSatisfyingVersion(npmPackage, ver)) {
-                    npmPackage.versions.add(latestSatisfyingVersion);
+    function addAllVersions(name) {
+        return new Promise(function (resolve) {
+            npm.getVersionsOf(name)
+                .spread((name, versions) => {
+                    let npmPackage = _.find(npmPackages, {name: name});
 
-                    if (npmPackage.downloaded) {
-                        npm.downloadTarBallOf(npmPackage.name, [latestSatisfyingVersion], program.output)
+                    npmPackage.versions = _.union(npmPackage.versions, versions);
+
+                    resolve();
+                });
+        })
+    }
+
+    function addDependencies(name) {
+        if (opts.dependencies) {
+            winston.info(`Adding dependencies for ${name}`);
+
+            return npm.getDependenciesOf(name)
+                .then((dependencies) => {
+                    let promises = [];
+                    _.forOwn(dependencies, (v, p) => promises.push(addLatestSatisfyingVersionOf(p, v)));
+
+                    return Promise.all(promises)
+                });
+        }
+
+        return Promise.resolve();
+    }
+
+    function addDevDependencies(name) {
+        if (opts.devDependencies) {
+            winston.info(`Adding dev dependencies for ${name}`);
+
+            return npm.getDevDependenciesOf(name)
+                .then((dependencies) => {
+                    let promises = [];
+                    _.forOwn(dependencies, (v, p) => promises.push(addLatestSatisfyingVersionOf(p, v)));
+
+                    return Promise.all(promises)
+                });
+        }
+
+        return Promise.resolve();
+    }
+
+    function addLatestSatisfyingVersionOf(packageName, ver) {
+        return npm.getVersionsOf(packageName)
+            .spread((name, versions) => {
+                let npmPackage = _.find(npmPackages, {name: packageName});
+                let latestSatisfyingVersion = versionUtils.getLatestSatisfyingVersion(versions, ver);
+
+                if (!npmPackage) {
+                    addNewPackage(packageName, latestSatisfyingVersion);
+                } else {
+                    if (!versionUtils.packageContainsSatisfyingVersion(npmPackage, ver)) {
+                        npmPackage.versions.add(latestSatisfyingVersion);
+
+                        if (npmPackage.downloaded) {
+                            npm.downloadTarBallOf(npmPackage.name, [latestSatisfyingVersion], opts.output)
+                        }
                     }
                 }
-            }
-        })
-}
-function getLatestSatisfyingVersion(versions, ver) {
-    if (validUrl.isUri(ver)){
-        if (ver.startsWith("git:")){
-            return ver.replace("git:", "https:").replace(".git", "/archive/master.zip");
-        }
-        return ver;
+            })
     }
 
-    return semver.maxSatisfying(versions, parseVersion(ver));
-}
+    function addNewPackage(npmPackage, latestSatisfyingVersion) {
+        npmPackages.push({name: npmPackage, versions: new Set([latestSatisfyingVersion]), downloaded: false});
+    }
 
-function parseVersion(ver) {
-    return ver == 'latest' ? ">0.0.0" : ver;
-}
+    function downloadTarBall(name) {
+        let npmPackage = _.find(npmPackages, {name: name});
 
-function addNewPackage(npmPackage, latestSatisfyingVersion) {
-    npmPackages.push({name: npmPackage, versions: new Set([latestSatisfyingVersion]), downloaded: false});
-}
-
-function packageContainsSatisfyingVersion(npmPackage, ver) {
-    return semver.maxSatisfying(Array.from(npmPackage.versions).filter(semver.valid), ver);
-}
-
-function addAllVersions(name) {
-    return new Promise(function (resolve) {
-        npm.getVersionsOf(name)
-            .spread((name, versions) => {
-                let npmPackage = _.find(npmPackages, {name: name});
-
-                npmPackage.versions = _.union(npmPackage.versions, versions);
-
-                resolve();
+        return npm.downloadTarBallOf(name, npmPackage.versions, opts.output)
+            .catch((e) => {
+                winston.error(`${e.message} \n Was unable to download ${name}`);
+            })
+            .finally(() => {
+                winston.info(`Finished downloading ${name}.`);
+                npmPackage.downloaded = true;
             });
-    })
-}
+    }
 
-function downloadTarBall(name) {
-    let npmPackage = _.find(npmPackages, {name: name});
-
-    return npm.downloadTarBallOf(name, npmPackage.versions, program.output)
-        .catch((e) => {
-            winston.error(`${e.message} \n Was unable to download ${name}`);
-        })
-        .finally(() => {
-            winston.info(`Finished downloading ${name}.`);
-            npmPackage.downloaded = true;
-        });
-}
-
-nextPackage();
+    nextPackage();
+};
