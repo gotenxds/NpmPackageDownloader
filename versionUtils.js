e96ef5300e4f4a65a27d71c860607e8a66db0f14
John Doe <johndoe@example.com>
2015-10-23 16:24:12 +0300

    Moved semver logic to versionUtils.js.

---
+++ versionUtils.js
@@ -0,0 +1,28 @@
+let isUri = require('valid-url').isUri,
+    semver = require('semver');
+
+module.exports = {
+    getLatestSatisfyingVersion: function (ver) {
+        if (isUri(ver)) {
+            return parseUri(ver);
+        }
+
+        return semver.maxSatisfying(versions, parseVersion(ver));
+    },
+
+    packageContainsSatisfyingVersion: function (npmPackage, ver) {
+        return semver.maxSatisfying(Array.from(npmPackage.versions).filter(semver.valid), ver);
+    }
+};
+
+function parseUri(uri){
+    if (uri.startsWith("git:")) {
+        return uri.replace("git:", "https:").replace(".git", "/archive/master.zip");
+    }
+
+    return uri;
+}
+
+function parseVersion(ver) {
+    return ver == 'latest' ? ">0.0.0" : ver;
+}
