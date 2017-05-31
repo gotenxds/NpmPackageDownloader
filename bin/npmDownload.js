683ceb95dd039fc82ef48c2b5d4a7f533f027d0d
shayalaluf <shayalalu@gmail.com>
2017-05-31 20:29:20 +0300

    add option to download from json file

--- bin/npmDownload.js
+++ bin/npmDownload.js
@@ -2,6 +2,7 @@
 "use strict";
 let program = require('commander'),
     packages = require('../coercions/packagesCoercion'),
+    packagesFromJsonFile = require('../coercions/packagesFromJsonFileCoercion'),
     app = require("../index.js"),
     winston = require('winston'),
     winstonConfigurator = require('../winstonConfiguration');
@@ -10,6 +11,7 @@ let program = require('commander'),
 program
     .version('0.1.4')
     .option('-p, --packages [packages]', 'A list of space separated [packages].', packages)
+    .option('-j, --packagesFromJsonFile [jsonFilePath]', 'path to package.json file.', packagesFromJsonFile)
     .option('-d, --dependencies', 'Download dependencies.')
     .option('-e, --devDependencies', 'Download dev dependencies.')
     .option('-a, --allVersions', 'Download all versions of each package.')
@@ -17,7 +19,7 @@ program
     .option('-7, --zipIt', '7Zips the downloaded files.')
     .parse(process.argv);
 
-if (!program.packages) {
+if (!program.packages && !packagesFromJsonFile) {
     winston.error("No npmPackage were given!");
     process.exit();
 }
