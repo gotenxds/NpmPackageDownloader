08d457ff97a6d83b1816d206b625ad9f05549513
shayalaluf <shayalalu@gmail.com>
2017-05-31 20:50:29 +0300

    read dependencies and devDependencies from json file create a list of dependencies in the right format

---
+++ coercions/packagesFromJsonFileCoercion.js
@@ -0,0 +1,27 @@
+'use strict';
+let fs = require('fs'),
+    packages = require('./packagesCoercion');
+
+module.exports = function (jsonFilePath) {
+    const fileContent = JSON.parse(fs.readFileSync(jsonFilePath, "utf8"));
+    const dependencies = attachNameAndVersion(fileContent.dependencies);
+    const devDependencies = attachNameAndVersion(fileContent.devDependencies);
+
+    return packages(dependencies + devDependencies);
+};
+
+function attachNameAndVersion(jsonObject) {
+    let orderedDependencies = '';
+
+    for (const dependency in jsonObject) {
+        let version = jsonObject[dependency];
+
+        if (version[0] !== '@') {
+            version = `@${version}`;
+        }
+
+        orderedDependencies += `${dependency}${version} `;
+    }
+
+    return orderedDependencies;
+}
